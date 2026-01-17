6aee76a4773514f9dbd03d592f3dd0fda7ddafb1
JaskaranSaini05 <JaskaranSaini092@gmail.com>
2026-01-17 22:04:10 +0530

    java script practice

---
+++ 03_conversionOperation.js
@@ -0,0 +1,15 @@
+let score = "3332aaa"
+
+console.log(typeof score);
+
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+let  isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// 1 => true;
+// 0 => false;
+//"hitesh" => true
