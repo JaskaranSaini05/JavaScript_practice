0d5dc0a6b737936e8810697145ee220a71df448d
JaskaranSaini05 <JaskaranSaini092@gmail.com>
2026-01-17 21:46:17 +0530

    Added Data Types

---
+++ 02_dataTypes.js
@@ -0,0 +1,14 @@
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// Below are primitive data types 
+// number => 2 to power 53
+// bigint =>mainly used for big number like in stock market
+// string => true/false
+// null => standalone value
+// undefined => not put any value eg let age;
+// symbol => unique
+
+console.log(typeof name);
