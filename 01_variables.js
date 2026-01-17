8bbed88433b03a44caf956749da1a790037188a3
JaskaranSaini05 <JaskaranSaini092@gmail.com>
2026-01-17 21:24:29 +0530

    Added let const var practice

---
+++ 01_variables.js
@@ -0,0 +1,15 @@
+const accountId =14453
+let accountEmail = "hitesh@google.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+let accountState;
+
+
+// accountId = 2  // not allowed
+accountEmail = "hccc22.com"
+accountPassword = "233223"
+accountCity = "bhopal"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
