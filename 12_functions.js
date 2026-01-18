63397ac306f82decb248dcd60be7d765875e90a3
JaskaranSaini05 <JaskaranSaini092@gmail.com>
2026-01-18 16:24:35 +0530

    Functions In  JavaScript

---
+++ 12_functions.js
@@ -0,0 +1,11 @@
+function sayMyName(){
+console.log("Jaskaran");
+}
+
+sayMyName()
+sayMyName()
+
+function addTwoNumbers(number1 ,number2){
+  console.log(number1 + number2);
+}
+addTwoNumbers(300,4)
