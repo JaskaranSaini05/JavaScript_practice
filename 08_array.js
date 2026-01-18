4e3d34fecec72ad3a4cb2818ca67666130979d37
JaskaranSaini05 <JaskaranSaini092@gmail.com>
2026-01-18 11:03:49 +0530

    Array Practice

---
+++ 08_array.js
@@ -0,0 +1,11 @@
+const myArr = [0,1,2,3,4,5,6,7,8]
+
+// Array methods
+
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
+myArr.unshift(9)
+myArr.shift(9)
+console.log(myArr.includes(8));
+console.log(myArr);
---
+++ 09_array.js
@@ -0,0 +1,10 @@
+const marvel_heros = ["thor","spiderman","Ironman"]
+const dc_heros = ["superman","flash","batman"]
+
+//marvel_heros.push(dc_heros);
+console.log(marvel_heros)
+console.log(marvel_heros)
+
+
+const all_new_heros = [...marvel_heros,...dc_heros]
+console.log(all_new_heros);
