59017898f16093bfbae4e4add98fefc4c9272d96
JaskaranSaini05 <JaskaranSaini092@gmail.com>
2026-01-19 23:38:47 +0530

    Control Flow in JavaScript

---
+++ 16_controlflow.js
@@ -0,0 +1,41 @@
+//if
+
+const isUserLoggeIn = true
+const temperature = 32
+if(temperature < 50){
+console.log("less than 50")
+}
+else {
+    console.log("greater then 50")
+}
+
+const score = 200
+
+if (score > 100) {
+  const power = "fly";
+  console.log(`User Power: ${power}`);
+}
+
+const balance = 1000
+
+if (balance > 500) console.log("test");
+
+if (balance < 500) {
+console.log("less than 500");
+}
+else if (balance < 750) {
+    console.log("less than 750");
+}
+else {
+    console.log("less than 1200");
+}
+
+//
+
+const userLoggeIn = true
+const debitCard = true
+if(userLoggeIn && debitCard) {
+
+    console.log("Allow to buy coursr");
+}
+
